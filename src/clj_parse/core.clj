9bcef0c40994711cf31114b5e5b3748b9d8b452d
Aaron Craelius <aaronc+git@practivist.org>
2012-02-18 01:34:47 -0500

    Moved some tests of private functions to core.clj.  Created some basic tests of generating the parser grammar in the parser grammar.

--- src/clj_parse/core.clj
+++ src/clj_parse/core.clj
@@ -230,3 +230,40 @@ using a grammar defined within Clojure."
 
 (defn parser [& forms] (first (parser-expr forms)))
 (def ||> parser)
+
+
+;; Inline tests for testing private functions
+(deftest test-match1
+  (let [m (partial match (match1 keyword?))]
+    (is (= (m [[:test] []]) [[] [:test]]))
+    (is (= (m [[5] []]) nil))))
+
+(deftest test-match1?
+  (let [m (partial match (match1? keyword?))]
+    (is (= (m [[:test] []]) [[] [:test]]))
+    (is (= (m [[5] []]) [[5] []]))))
+
+(deftest test-match*
+  (let [m (partial match (match* keyword?))]
+    (is (= (m [[:a :b :c 4 5 6] []])
+           [[4 5 6] [:a :b :c]]))
+    (is (= (m [[1 2] []]) [[1 2] []]))))
+
+(deftest test-match+
+  (let [m (partial match (match+ keyword?))]
+    (is (= (m [[:a :b :c 4 5 6] []])
+           [[4 5 6] [:a :b :c]]))
+    (is (= (m [[1 2] []]) nil))))
+
+(deftest test-match-seq
+  (let [m (partial match (MatchSeq. nil [ (match* keyword?) (match1 symbol?) (match1? symbol?) (match+ number?)]))]
+    (is (= (m [[:a :b :c 'x 1 2 3] []]) [[] [:a :b :c 'x 1 2 3]]))
+    (is (= (m [[:a :b :c 'x 'y 1 2 3] []]) [[] [:a :b :c 'x 'y 1 2 3]]))
+    (is (= (m [[:a :b :c 'x 'y 'z 1 2 3] []]) nil))))
+
+(deftest test-match-or
+  (let [m (partial match (MatchOr. nil [ (match+ keyword?) (match1 symbol?) (match+ number?)]))]
+    (is (= (m [[:a :b :c] []]) [[] [:a :b :c]]))
+    (is (= (m [['x 1] []]) [[1] ['x]]))
+    (is (= (m [[1 2 3] []]) [[] [1 2 3]]))
+    (is (= (m [["xyx"] []]) nil))))
--- test/clj_parse/test/core.clj
+++ test/clj_parse/test/core.clj
@@ -1,74 +1,12 @@
 (ns clj-parse.test.core
   (:use [clj-parse.core])
-  (:use [clojure.test])
-  (:import [clj-parse.core Match? Match*]))
+  (:use [clojure.test]))
 
-(defn match1? [f] (Match?. (match1 f)))
+(def MatchExpr (||> [(||> [any (||> [(eq :+)] [(eq :*)] [(eq :?)]) :?]) :+]))
 
-(defn match* [f] (Match*. (match1 f)))
+(defn test-match-expr [expr]
+  (is (= (MatchExpr expr) expr)))
 
-(defn match+ [f] (Match+. (match1 f)))
-
-(defn match-seq [matchers] (MatchSeq. matchers))
-
-(defn match-or [matchers] (MatchOr. matchers))
-
-(defn match-sub-seq [matcher] (MatchSubSeq. matcher))
-
-(defn match-transform [matcher transform] (MatchTransformer. matcher transform))
-
-(deftest test-match1
-  (let [m (partial match (match1 keyword?))]
-    (is (= (m [[:test] []]) [[] [:test]]))
-    (is (= (m [[5] []]) nil))))
-
-(deftest test-match1?
-  (let [m (partial match (match1? keyword?))]
-    (is (= (m [[:test] []]) [[] [:test]]))
-    (is (= (m [[5] []]) [[5] []]))))
-
-(deftest test-match*
-  (let [m (partial match (match* keyword?))]
-    (is (= (m [[:a :b :c 4 5 6] []])
-           [[4 5 6] [:a :b :c]]))
-    (is (= (m [[1 2] []]) [[1 2] []]))))
-
-(deftest test-match+
-  (let [m (partial match (match+ keyword?))]
-    (is (= (m [[:a :b :c 4 5 6] []])
-           [[4 5 6] [:a :b :c]]))
-    (is (= (m [[1 2] []]) nil))))
-
-(deftest test-match-seq
-  (let [m (partial match (match-seq [ (match* keyword?) (match1 symbol?) (match1? symbol?) (match+ number?)]))]
-    (is (= (m [[:a :b :c 'x 1 2 3] []]) [[] [:a :b :c 'x 1 2 3]]))
-    (is (= (m [[:a :b :c 'x 'y 1 2 3] []]) [[] [:a :b :c 'x 'y 1 2 3]]))
-    (is (= (m [[:a :b :c 'x 'y 'z 1 2 3] []]) nil))))
-
-(deftest test-match-or
-  (let [m (partial match (match-or [ (match+ keyword?) (match1 symbol?) (match+ number?)]))]
-    (is (= (m [[:a :b :c] []]) [[] [:a :b :c]]))
-    (is (= (m [['x 1] []]) [[1] ['x]]))
-    (is (= (m [[1 2 3] []]) [[] [1 2 3]]))
-    (is (= (m [["xyx"] []]) nil))))
-
-(comment (deftest test-match-expr
-           (let [test-exp (fn [e] (is (= (match-match-expr e) e)))]  
-             (test-exp [keyword? symbol? '?]) 
-             (test-exp '[|| (keyword? symbol? ?)
-                         (number? symbol? number? (|| (number? ?) (string? ?)))]))))
-
-(def MatchOccurenceExpr (matcher
-  [any (|| [(eq +) => (constantly match+)]
-           [(eq *) => (constantly match*)]
-           [(eq ?) => (constantly match1?)]
-           =>) (default match1)] => (fn [expr matchx] (matchx expr))))
-
-;(def SubSequenceMatchexpr (matcher [vector? => ]))
-
-(def MatchTransformExpr (matcher [MatchOccurenceExpr (eq =>) any]
-                                 => (fn [expr transform] (match-transform expr transform)))) 
-
-(def PrimaryMatchExpr (|| MatchTransformExpr MatchOccurenceExpr))
-  
-(def MatchExpr (matcher [PrimaryMatchExpr +] => (fn [matchers] (match-seq matchers))))
+(deftest test-expressions
+  (test-match-expr [keyword? :+])
+  (test-match-expr [string? keyword? :+ number? :*]))
