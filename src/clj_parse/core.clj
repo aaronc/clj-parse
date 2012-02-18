dfc7b3525542a3f1a70794a4604d2e04a8356933
Aaron Craelius <aaronc+git@practivist.org>
2012-02-18 01:20:06 -0500

    Big refactoring. Switched from using +, *, ?, and => to the keywords :+, :*, :?, and :=>.  The parsers now feel easier to read and less hackish.  Am now using a single ||> macro instead of || and >>>.  Greatly improved debugging support with nested logging support and named parsers.

--- src/clj_parse/core.clj
+++ src/clj_parse/core.clj
@@ -5,64 +5,109 @@
 
 (def ^:dynamic **debug** false)
 
-(defn- log [& forms] (when **debug** (println (apply str forms))))
+(defn- indent [n] (apply str (repeat (dec n) "-")))
+
+(defn- thread-local* [init] ;; From https://github.com/flatland/useful
+  (let [generator (proxy [ThreadLocal] []
+                    (initialValue [] init))]
+        (reify clojure.lang.IDeref
+         (deref [this]
+         (.get generator)))))
+
+(def log-stack (thread-local* (atom (list))))
+
+(defn- log-top [ret]
+  (when **debug**
+    (let [top (peek @@log-stack)
+          depth (count @@log-stack)
+          pre (indent depth)]
+      (when (or ret (> depth 0))
+        (if ret
+          (println (str pre top " => " ret))
+          (println (str pre top ":")))))))
+        
+(defn- push-log [this name]
+  (do (log-top nil)
+      (let [msg (str "<" (.getSimpleName (class this)) " " (or name "Unknown") ">")]
+        (swap! @log-stack conj msg))))
+
+(defn- pop-log [this name ret]
+  (do (log-top ret)
+      (swap! @log-stack pop)
+      ret))
+
+(defn- clear-log-stack [] (swap! @log-stack (fn [_] (list))))
+
+(defmacro logged [this name & body]
+  `(try (push-log ~this ~name)
+        (let [ret# (do ~@body)]
+        (pop-log ~this name ret#)
+         ret#)
+  (catch Throwable e#
+    (clear-log-stack)
+    (throw e#))))
 
 (defn- done? [ctxt] (empty? (ctxt 0)))
 
-(defrecord Match1 [test-fn]
-IMatcher
-(match [this [coll res]]
-(when-let [x (first coll)]
-  (if (test-fn x)
-  [(rest coll) (conj res x)]
-  nil))))
+(defrecord Match1 [name test-fn]
+  IMatcher
+  (match [this [coll res]]
+    (logged this name
+        (when-let [x (first coll)]
+          (if (test-fn x)
+            [(rest coll) (conj res x)]
+            nil)))))
 
-(defn match1 [f] (if (satisfies? IMatcher f) f (Match1. f)))
+(defn match1
+  ([name f] (if (satisfies? IMatcher f) f (Match1. name f)))
+  ([f] (match1 nil f)))
 
-(defrecord Match? [matcher] IMatcher (match [this ctxt] (or (match matcher ctxt) ctxt)))
+(defrecord Match? [name matcher] IMatcher (match [this ctxt] (logged this name (or (match matcher ctxt) ctxt))))
 
 (defn- do-match* [matcher ctxt]
     (loop [last-ctxt ctxt]
-    (log "do-match* : " matcher " @ " last-ctxt)
     (if (done? last-ctxt)
       last-ctxt
       (if-let [cur-ctxt (match matcher last-ctxt)]
                             (if (= cur-ctxt last-ctxt) last-ctxt (recur cur-ctxt))
                             last-ctxt))))
 
-(defrecord Match* [matcher] IMatcher (match [this ctxt] (do-match* matcher ctxt)))
+(defrecord Match* [name matcher] IMatcher (match [this ctxt] (logged this name (do-match* matcher ctxt))))
 
-(defrecord Match+ [matcher]
+(defrecord Match+ [name matcher]
 IMatcher (match [this ctxt]
-(let [new-ctxt (do-match* matcher ctxt)]
- (if (= ctxt new-ctxt) nil new-ctxt))))
+  (logged this name 
+    (let [new-ctxt (do-match* matcher ctxt)]
+      (if (= ctxt new-ctxt) nil new-ctxt)))))
 
-(defn- match1? [f] (Match?. (match1 f)))
+(defn- match1?
+  ([name f] (Match?. name (match1 f)))
+  ([f] (match1? nil f)))
 
-(defn- match* [f] (Match*. (match1 f)))
+(defn- match* [f] (Match*. nil (match1 f)))
 
-(defn- match+ [f] (Match+. (match1 f)))
+(defn- match+ [f] (Match+. nil (match1 f)))
 
-(defrecord MatchSeq [matchers]
+(defrecord MatchSeq [name matchers]
 IMatcher
 (match [this ctxt]
-(loop [m (first matchers)
-       more (rest matchers)
-       last-ctxt ctxt]
-  (log this " : " m " @ " last-ctxt)
-  (if m (if-let [new-ctxt (match m last-ctxt)] (recur (first more) (rest more) new-ctxt) nil)
-      last-ctxt))))
+  (logged this name
+          (loop [m (first matchers)
+                 more (rest matchers)
+                 last-ctxt ctxt]
+            (if m (if-let [new-ctxt (match m last-ctxt)] (recur (first more) (rest more) new-ctxt) nil)
+                last-ctxt)))))
 
-(defrecord MatchOr [matchers]
+(defrecord MatchOr [name matchers]
 IMatcher
 (match [this ctxt]
-(loop [m (first matchers)
-       more (rest matchers)]
-  (log this " : " m)
-  (when m
-    (if-let [new-ctxt (match m ctxt)]
-      new-ctxt
-      (recur (first more) (rest more)))))))
+  (logged this name
+          (loop [m (first matchers)
+                 more (rest matchers)]
+            (when m
+              (if-let [new-ctxt (match m ctxt)]
+                new-ctxt
+                (recur (first more) (rest more))))))))
 
 (defn- matches-entirely? [[coll res]]
   (when (empty? coll) res))
@@ -76,79 +121,112 @@ IMatcher
     (when-let [sub-ctxt (match matcher [x []])]
       (when (done? sub-ctxt) [more (conj res (second sub-ctxt))])))))
 
-(defrecord MatchSubSeq [matcher]
-IMatcher (match [this ctxt] (do-match-sub-seq matcher ctxt)))
+(defrecord MatchSubSeq [name matcher]
+  IMatcher (match [this ctxt] (logged this name (do-match-sub-seq matcher ctxt))))
 
-(defrecord MatchTransformer [matcher transform]
+(defrecord MatchTransformer [name matcher transform]
 IMatcher
 (match [this ctxt]
-(when-let [new-ctxt (match matcher ctxt)]
-  (let [orig-res (second ctxt)
-        start (count orig-res)
-        new-res (second new-ctxt)
-        end (count new-res)
-        transformed (transform (subvec new-res start end))]
-    [(first new-ctxt)
-     (if transformed
-       (if (sequential? transformed) (into orig-res transformed) (conj orig-res transformed))
-       orig-res)]))))
+  (logged this name
+          (when-let [new-ctxt (match matcher ctxt)]
+            (let [orig-res (second ctxt)
+                  start (count orig-res)
+                  new-res (second new-ctxt)
+                  end (count new-res)
+                  transformed (transform (subvec new-res start end))]
+              [(first new-ctxt)
+               (if transformed
+                 (if (sequential? transformed) (into orig-res transformed) (conj orig-res transformed))
+                 orig-res)])))))
 
 ;; Convenience mini-language definitions
 (def any (constantly true))
 
 (def ignore (constantly nil))
 
-(def ? (Object.))
-
-(def => (Object.))
-
 (defn eq [x] (fn [y] (= x y)))
 
 (defn default [value] (fn [res] (if (empty? res) value res)))
 
-(defn unary-op [from to] (MatchTransformer. (match1 (eq from)) (constantly to)))
+(defn unary-op [from to] (MatchTransformer. (str "TransformMatchOperator " from) (match1 (eq from)) (constantly to)))
 
 (declare match-expr)
 
 (defrecord SubSeqMatchExpr []
   IMatcher
-  (match [this ctxt] (do-match-sub-seq match-expr ctxt)))
+  (match [this ctxt] (logged "SubSeqMatchExpr" (do-match-sub-seq match-expr ctxt))))
 
-(def match-sub-seq-expr (MatchTransformer. (SubSeqMatchExpr.) (fn [[matchers]] (MatchSubSeq. (MatchSeq. matchers)))))
+(def match-sub-seq-expr (MatchTransformer. "TransformSubSeqMatchExpr" (SubSeqMatchExpr.) (fn [[matchers]] (MatchSubSeq. nil (MatchSeq. nil matchers)))))
 
-(def match-atom (MatchOr. [match-sub-seq-expr (match1 any)]))
+(def match-atom
+  (MatchSeq. "MatchAtomWithName"
+    [(MatchOr. "MatchAtom" [match-sub-seq-expr (match1 any)])
+     (MatchTransformer. "DefaultMatchName" (match1? "MatchName" string?) (default [nil]))]))
 
 (def match-occurrence-expr
-(MatchTransformer.
-(MatchSeq. [match-atom
-           (MatchTransformer.
-            (Match?. (MatchOr. [(unary-op + match+) (unary-op * match*) (unary-op ? match1?)]))
-            (default match1))])
-(fn [[expr modifier]] (modifier expr))))
+  (MatchTransformer. "TransformMatchOccurrenceExpr"
+    (MatchSeq. "MatchOccurrenceExpr"
+      [match-atom
+       (MatchTransformer. "TransformMatchOperator?"
+         (Match?. "MatchOperator?"
+           (MatchOr. "MatchOperators"
+             [(unary-op :+ match+) (unary-op :* match*) (unary-op :? match1?)]))
+         (default match1))])
+    (fn [[expr name modifier]] (assoc (modifier expr) :name name))))
 
 (def match-transform-expr
-(MatchTransformer.
-(MatchSeq. [match-occurrence-expr (MatchTransformer. (match1 (eq =>)) ignore) (match1 any)])
-(fn [[matcher transform]] (MatchTransformer. matcher transform))))
+  (MatchTransformer. "TransformMatchTransformExpr"
+    (MatchSeq. "MatchTransformExpr" [match-occurrence-expr (MatchTransformer. "MatchTransformOperator" (match1 (eq :=>)) ignore) (match1 any)])
+    (fn [[matcher transform]] (MatchTransformer. nil matcher transform))))
 
-(def primary-match-expr (MatchOr. [match-transform-expr match-occurrence-expr]))
+(def primary-match-expr (MatchOr. "PrimaryMatchExpr" [match-transform-expr match-occurrence-expr]))
 
-(def match-expr (Match+. primary-match-expr))
+(def match-expr (Match+. "MatchExpr" primary-match-expr))
 
 (defn matcher
-([forms] (MatchSeq. (matches-entirely? (match match-expr [forms []]))))
-([forms _ transform] (MatchTransformer. (matcher forms) transform)))
+([forms] (MatchSeq. "Matcher" (matches-entirely? (match match-expr [forms []]))))
+([forms _ transform] (MatchTransformer. nil (matcher forms) transform)))
 
 (defrecord Parser [matcher]
 IMatcher (match [this ctxt] (match matcher ctxt))
 clojure.lang.IFn (invoke [this coll] (second (match this [coll []]))))
 
-(defn parser
-"Creates a parser for parsing and transforming Clojure symbols
+(defn >>>
+  "Creates a parser for parsing and transforming Clojure symbols
 using a grammar defined within Clojure."
-([forms] (Parser. (matcher forms)))
-([forms _ transform] (Parser. (matcher forms _ transform))))
+  ([forms] (Parser. (matcher forms)))
+  ([forms _ transform] (Parser. (matcher forms _ transform))))
 
-(def >>> parser)
+(defn || [& forms] (Parser. (MatchOr. "MatchOr" (map #(if (satisfies? IMatcher %) % (matcher %)) forms))))
 
-(defn || [& forms] (Parser. (MatchOr. (map #(if (satisfies? IMatcher %) % (matcher %)) forms))))
+(def parser-expr
+ (Parser.
+  (MatchTransformer. "ParserGen"
+    (MatchSeq. "ParserExpr"
+      [(MatchTransformer. "DefaultParseName" (match1? "ParserName" string?) (default [nil]))
+       (MatchTransformer. "GroupParseExpressions"
+         (Match+. "ParseExpressions"
+           (MatchOr. "ParseExpression"
+             [(match1 "MatcherRef" (partial satisfies? IMatcher))
+              (MatchTransformer. "MatchVectorSeq"
+                (MatchSubSeq. "MatchVector" match-expr)
+                (fn [matchers] (MatchSeq. nil (do (assert (= 1 (count matchers))) (first matchers)))))]))
+         (fn [parsers] [parsers]))
+       (MatchTransformer. "DefaultParserTransform"
+         (match1? "ParserTransform?"
+           (MatchSeq. "ParserTransformSeq"
+             [(MatchTransformer. "Ignore :=>" (match1 "TransformOperator" (eq :=>)) ignore)
+              (match1 "TransformExpression" any)]))
+         (default [nil]))])
+    (fn [[name parsers transform]]
+        (let [parser
+              (if (= 1 (count parsers))
+                (assoc (first parsers) :name name)
+                (MatchOr. name parsers))]
+          (Parser.
+           (if transform
+             (MatchTransformer. nil parser transform)
+             parser)))))))
+
+(defn parser [& forms] (first (parser-expr forms)))
+(def ||> parser)
