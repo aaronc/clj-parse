0ec52a98a189b2d2aec633ef74d58acaa92fa812
Aaron Craelius <aaronc+git@practivist.org>
2012-02-18 22:24:30 -0500

    Have crazy-macro2 working in clj-parse.test.pseudoebnf

--- src/clj_parse/pseudoebnf.clj
+++ src/clj_parse/pseudoebnf.clj
@@ -17,12 +17,15 @@
                                  (mconstantly m? (eq :?)))))
               (mdefault [nil] (m? (mseq "MatchTransformExpr" (mignore (eq :=>)) identity))))))))
 
+(defn >>> [& forms] (first (MatchExpr forms)))
+
 (def MatchOrExpr
   (mapply
    (fn [name matcher] (if name (assoc matcher :name name) matcher))
    (mseq
        (mdefault [nil] (m? string?))
-       (mapply mor (m+ "OrExpression" (mapply m1 identity))))))
+       (mapply mor (m+ "OrExpression" (mapply #(if (satisfies? IMatcher %) %
+                                                   (if (sequential? %) (apply >>> %)
+                                                       (m1 %))) identity))))))
 
-(defn >>> [& forms] (first (MatchExpr forms)))
 (defn || [& forms] (first (MatchOrExpr forms)))
--- test/clj_parse/test/pseudoebnf.clj
+++ test/clj_parse/test/pseudoebnf.clj
@@ -12,22 +12,21 @@
   (test-match-expr [keyword? :+])
   (test-match-expr [string? keyword? :+ number? :*]))
 
-(comment
-  (def crazy-grammar2
-    (||> [(||> [(||> [(eq 'Say) :=> (constantly #(str % ". "))]
-                     [(eq 'Shout) :=> (constantly #(.toUpperCase (str % "! ")))]
-                     [(eq 'Whisper) :=> (constantly #(.toLowerCase (str % "... ")))])
-                #(and (symbol? %) (not (= '. %))) :+ :=> #(apply str (interpose " " %))
-                (||> [number? (eq 'times) :=> ignore]) :? :=> (default 1)
-                (eq '.) :=> ignore] :=> vector) :+]))
+(def crazy-grammar2
+  (>>> (>>> (>>> (||  [(eq 'Say) :=> (constantly #(str % ". "))]
+                      [(eq 'Shout) :=> (constantly #(.toUpperCase (str % "! ")))]
+                      [(eq 'Whisper) :=> (constantly #(.toLowerCase (str % "... ")))])
+                 #(and (symbol? %) (not (= '. %))) :+ :=> #(apply str (interpose " " %&))
+                 (>>> number? (eq 'times) :=> ignore) :? :=> (default 1)
+                 (eq '.) :=> ignore) :=> group) :+))
 
-  (defmacro crazy-macro2 [& words]
-    (let [sentences (crazy-grammar2 words)
-          actions (for [[f s n] sentences]
-                    `(for [i# (range ~n)] (~f ~s)))]
-      `(apply str (flatten [~@actions]))))
+(defmacro crazy-macro2 [& words]
+  (let [sentences (crazy-grammar2 words)
+        actions (for [[f s n] sentences]
+                  `(for [i# (range ~n)] (~f ~s)))]
+    `(apply str (flatten [~@actions]))))
 
-  (deftest test-crazy-macro2
-    (is (= (crazy-macro2 Say Hello 3 times . Shout hello world 2 times . Whisper goodbye .)
-           "Hello. Hello. Hello. HELLO WORLD! HELLO WORLD! goodbye... "))))
+(deftest test-crazy-macro2
+  (is (= (crazy-macro2 Say Hello 3 times . Shout hello world 2 times . Whisper goodbye .)
+         "Hello. Hello. Hello. HELLO WORLD! HELLO WORLD! goodbye... ")))
 
