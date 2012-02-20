8f1549d8e58ab862289d798f83aee883e1c537d6
Aaron Craelius <aaronc+git@practivist.org>
2012-02-20 14:33:27 -0500

    Updated project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clj-parse "0.4.3"
+(defproject clj-parse "0.4.4-SNAPSHOT"
   :description "A library for parsing Clojure symbols in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-autodoc "0.9.0"]
-                     [lein-marginalia "0.7.0-SNAPSHOT"]]
-  :aot [clj-parse.core])
+                     [lein-marginalia "0.6.1"]])
