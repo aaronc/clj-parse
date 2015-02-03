a9810150f32b4e2d2e95acd177ffa64313e0a479
Aaron Craelius <aaroncraelius@gmail.com>
2015-02-03 10:54:57 -0800

    Updated Clojure version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-parse "0.4.4-SNAPSHOT"
   :description "A library for parsing Clojure symbols in Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :dev-dependencies [[lein-autodoc "0.9.0"]
                      [lein-marginalia "0.6.1"]]
   :aot [clj-parse.core clj-parse.helpers clj-parse.pseudoebnf])
